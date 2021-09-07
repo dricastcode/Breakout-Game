4e0f6cf46002207046115a7ae0928b65e2c50f1c
Adrian <64158136+dricastcode@users.noreply.github.com>
2021-09-07 15:33:20 -0700

    Building bricks for destruction 😈

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const ctx = canvas.getContext('2d')
 let score = 0
 
 const brickRowCount = 9
+const brickColumnCount = 5
 
 // Create ball props
 const ball = {
@@ -28,6 +29,27 @@ const paddle = {
     dx: 0
 }
 
+// Create Brick props
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+}
+
+// Create Bricks
+const bricks = []
+for(let i = 0; i < brickRowCount; i++) {
+    bricks[i] = []
+    for(let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY
+        bricks[i][j] = { x, y, ...brickInfo }
+    }
+}
+
 // Draw ball on canvas
 function drawBall() {
     ctx.beginPath()
@@ -46,20 +68,34 @@ function drawPaddle() {
     ctx.closePath()
 }
 
-// Draw everything
-function draw() {
-    drawBall()
-    drawPaddle()
-    drawScore()
-}
-
-draw()
-
 // Draw score on canvas
 function drawScore() {
     ctx.font = '20px Arial'
     ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
 }
+
+// Draw bricks on canvas
+function drawBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath()
+            ctx.rect(brick.x, brick.y, brick.w, brick.h)
+            ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent'
+            ctx.fill()
+            ctx.closePath
+        })
+    })
+}
+
+// Draw everything
+function draw() {
+    drawBall()
+    drawPaddle()
+    drawScore()
+    drawBricks()
+}
+
+draw()
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'))
